c9eb911a6569671460f8e2c83df474285b4b0dd9
Marius Hornemann Tyson <MarHTys@gmail.com>
2026-04-20 23:46:26 +0200

    GPIO fuctions first attempt
    Den skal helt klart rettes og der er ikke nogen tests, men her er nogen functioner

---
+++ firmware/Project_Trace/Core/Inc/gpio_set.h
@@ -0,0 +1,14 @@
+/*
+ * gpio_set.h
+ *
+ *  Created on: 20 Apr 2026
+ *      Author: Marius H. Tyson
+ */
+
+#ifndef INC_GPIO_SET_H_
+#define INC_GPIO_SET_H_
+#include <main.h>
+#include <stm32h7xx_it.h>
+
+
+#endif /* INC_GPIO_SET_H_ */
