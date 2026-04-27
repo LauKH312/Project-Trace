b1b98c7331d4c1fad78f202c8c44bb4de00db89a
Palexander123 <alexandernyboolsen@yahoo.dk>
2026-04-27 12:19:35 +0200

    New project added

---
+++ firmware/Project_Trace/Core/Inc/test_suite.h
@@ -0,0 +1,13 @@
+/*
+ * test_suite.h
+ *
+ *  Created on: Apr 27, 2026
+ *      Author: Alexander
+ */
+
+#ifndef INC_TEST_SUITE_H_
+#define INC_TEST_SUITE_H_
+
+void test_suite(void);
+
+#endif /* INC_TEST_SUITE_H_ */
---
+++ firmware/Project_Trace/Core/Src/test_suite.c
@@ -0,0 +1,12 @@
+/*
+ * test_suite.c
+ *
+ *  Created on: Apr 27, 2026
+ *      Author: Alexander
+ */
+
+#include <test_suite.h>
+
+void test_suite(void) {
+	(void)0;
+}
