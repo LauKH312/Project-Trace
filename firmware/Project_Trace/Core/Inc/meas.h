4f24705f54e46041c5413ca5f1ddee3db5dc3229
LauKH312 <laurits.k.hillemann@gmail.com>
2026-06-09 17:29:05 +0200

    Update Meas API according to new Samplebuf format

--- firmware/Project_Trace/Core/Inc/meas.h
+++ firmware/Project_Trace/Core/Inc/meas.h
@@ -11,13 +11,13 @@
 #include <fixpoint.h>
 #include <stddef.h>
 
-fix9_23 meas_scale_sample(fix1_15 x, fix9_23 unit);
-fix9_23 meas_average(fix1_15* buf, size_t buffer_length, fix9_23 unit);
+fix9_23 meas_scale_sample(fix9_23 x);
+fix9_23 meas_average(fix9_23* buf, size_t buffer_length);
 
-fix9_23 meas_rms(fix1_15* buf, size_t buffer_length, fix9_23 unit);
-fix9_23 meas_max(fix1_15* buf, size_t buffer_length, fix9_23 unit);
-fix9_23 meas_min(fix1_15* buf, size_t buffer_length, fix9_23 unit);
+fix9_23 meas_rms(fix9_23* buf, size_t buffer_length);
+fix9_23 meas_max(fix9_23* buf, size_t buffer_length);
+fix9_23 meas_min(fix9_23* buf, size_t buffer_length);
 
-_Bool rising_edge(fix1_15* buf, size_t buffer_length, fix1_15 threshold, size_t* rising_edge_idx);
+_Bool rising_edge(fix9_23* buf, size_t buffer_length, fix9_23 threshold, size_t* rising_edge_idx);
 
 #endif /* INC_MEAS_H_ */
--- firmware/Project_Trace/Core/Src/meas.c
+++ firmware/Project_Trace/Core/Src/meas.c
@@ -8,24 +8,20 @@
 
 #include <meas.h>
 
-fix9_23 meas_scale_sample(fix1_15 x, fix9_23 unit) {
-    return fix9_23_mul(fix9_23_from_f1_15(x), unit);
-}
-
-fix9_23 meas_average(fix1_15* buf, size_t buffer_length, fix9_23 unit) {
+fix9_23 meas_average(fix9_23* buf, size_t buffer_length) {
     int64_t acc = 0;
     for (size_t i = 0; i < buffer_length; i++) {
-        const fix9_23 x = meas_scale_sample(buf[i], unit);
+        const fix9_23 x = buf[i];
         acc += x.raw;
     } 
     acc /= (int64_t) buffer_length;
     return FIX9_23_RAW((int32_t) acc);
 }
 
-fix9_23 meas_rms(fix1_15* buf, size_t buffer_length, fix9_23 unit) {
+fix9_23 meas_rms(fix9_23* buf, size_t buffer_length) {
     int64_t acc = 0;
     for (size_t i = 0; i < buffer_length; i++) {
-        const fix9_23 x = meas_scale_sample(buf[i], unit);
+        const fix9_23 x = buf[i];
         acc += fix9_23_sqr(x).raw;
     }
 
@@ -33,11 +29,11 @@ fix9_23 meas_rms(fix1_15* buf, size_t buffer_length, fix9_23 unit) {
     return fix9_23_sqrt(FIX9_23_RAW((int32_t) acc));
 }
 
-fix9_23 meas_max(fix1_15* buf, size_t buffer_length, fix9_23 unit) {
+fix9_23 meas_max(fix9_23* buf, size_t buffer_length) {
     fix9_23 max = FIX9_23_RAW(INT32_MIN);
 
     for (size_t i = 0; i < buffer_length; i++) {
-        const fix9_23 x = meas_scale_sample(buf[i], unit);
+        const fix9_23 x = buf[i];
         if (x.raw > max.raw) max = x;
     }
 
@@ -45,18 +41,18 @@ fix9_23 meas_max(fix1_15* buf, size_t buffer_length, fix9_23 unit) {
 
 }
 
-fix9_23 meas_min(fix1_15* buf, size_t buffer_length, fix9_23 unit) {
+fix9_23 meas_min(fix9_23* buf, size_t buffer_length) {
     fix9_23 min = FIX9_23_RAW(INT32_MAX);
 
     for (size_t i = 0; i < buffer_length; i++) {
-        const fix9_23 x = meas_scale_sample(buf[i], unit);
+        const fix9_23 x = buf[i];
         if (x.raw > min.raw) min = x;
     }
 
     return min;
 }
 
-_Bool rising_edge(fix1_15* buf, size_t buffer_length, fix1_15 threshold, size_t* rising_edge_idx) {
+_Bool rising_edge(fix9_23* buf, size_t buffer_length, fix9_23 threshold, size_t* rising_edge_idx) {
 	_Bool previous_below_thr = 0;
 	for (size_t i = 0; i < buffer_length; i++) {
 
