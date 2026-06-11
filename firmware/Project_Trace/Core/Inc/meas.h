b42a71ea951dc20d9e6b060320ea97bdac2ef6b0
LauKH312 <laurits.k.hillemann@gmail.com>
2026-06-11 20:30:48 +0200

    meas: Make inputs const, better name for rising_edge

--- firmware/Project_Trace/Core/Inc/meas.h
+++ firmware/Project_Trace/Core/Inc/meas.h
@@ -12,12 +12,12 @@
 #include <stddef.h>
 
 fix9_23 meas_scale_sample(fix9_23 x);
-fix9_23 meas_average(fix9_23* buf, size_t buffer_length);
+fix9_23 meas_average(const fix9_23* buf, size_t buffer_length);
 
-fix9_23 meas_rms(fix9_23* buf, size_t buffer_length);
-fix9_23 meas_max(fix9_23* buf, size_t buffer_length);
-fix9_23 meas_min(fix9_23* buf, size_t buffer_length);
+fix9_23 meas_rms(const fix9_23* buf, size_t buffer_length);
+fix9_23 meas_max(const fix9_23* buf, size_t buffer_length);
+fix9_23 meas_min(const fix9_23* buf, size_t buffer_length);
 
-_Bool rising_edge(fix9_23* buf, size_t buffer_length, fix9_23 threshold, size_t* rising_edge_idx);
+_Bool meas_rising_edge(const fix9_23* buf, size_t buffer_length, fix9_23 threshold, size_t* rising_edge_idx);
 
 #endif /* INC_MEAS_H_ */
--- firmware/Project_Trace/Core/Src/meas.c
+++ firmware/Project_Trace/Core/Src/meas.c
@@ -8,7 +8,7 @@
 
 #include <meas.h>
 
-fix9_23 meas_average(fix9_23* buf, size_t buffer_length) {
+fix9_23 meas_average(const fix9_23* buf, size_t buffer_length) {
     int64_t acc = 0;
     for (size_t i = 0; i < buffer_length; i++) {
         const fix9_23 x = buf[i];
@@ -18,7 +18,7 @@ fix9_23 meas_average(fix9_23* buf, size_t buffer_length) {
     return FIX9_23_RAW((int32_t) acc);
 }
 
-fix9_23 meas_rms(fix9_23* buf, size_t buffer_length) {
+fix9_23 meas_rms(const fix9_23* buf, size_t buffer_length) {
     int64_t acc = 0;
     for (size_t i = 0; i < buffer_length; i++) {
         const fix9_23 x = buf[i];
@@ -29,7 +29,7 @@ fix9_23 meas_rms(fix9_23* buf, size_t buffer_length) {
     return fix9_23_sqrt(FIX9_23_RAW((int32_t) acc));
 }
 
-fix9_23 meas_max(fix9_23* buf, size_t buffer_length) {
+fix9_23 meas_max(const fix9_23* buf, size_t buffer_length) {
     fix9_23 max = FIX9_23_RAW(INT32_MIN);
 
     for (size_t i = 0; i < buffer_length; i++) {
@@ -41,7 +41,7 @@ fix9_23 meas_max(fix9_23* buf, size_t buffer_length) {
 
 }
 
-fix9_23 meas_min(fix9_23* buf, size_t buffer_length) {
+fix9_23 meas_min(const fix9_23* buf, size_t buffer_length) {
     fix9_23 min = FIX9_23_RAW(INT32_MAX);
 
     for (size_t i = 0; i < buffer_length; i++) {
@@ -52,7 +52,7 @@ fix9_23 meas_min(fix9_23* buf, size_t buffer_length) {
     return min;
 }
 
-_Bool rising_edge(fix9_23* buf, size_t buffer_length, fix9_23 threshold, size_t* rising_edge_idx) {
+_Bool meas_rising_edge(const fix9_23* buf, size_t buffer_length, fix9_23 threshold, size_t* rising_edge_idx) {
 	_Bool previous_below_thr = 0;
 	for (size_t i = 0; i < buffer_length; i++) {
 
