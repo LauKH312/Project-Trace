ffebbe9c6779a6a85e02cffef94432e6726c0fd2
LauKH312 <laurits.k.hillemann@gmail.com>
2026-06-13 17:26:50 +0200

    Rewrite the `complex9_23_format` function

--- firmware/Project_Trace/Core/Src/complex.c
+++ firmware/Project_Trace/Core/Src/complex.c
@@ -105,29 +105,21 @@ Complex9_23 complex9_23_euler(fix9_23 x) {
     };
 }
 
+#define COMPLEX_FORMAT_BUF_LEN 16
+
 int complex9_23_format(Complex9_23 z, char* buffer, size_t len) {
-    int real_len = fix9_23_format(z.re, buffer,len);
-    if (real_len <= 0) return -1; // TODO snprintf error value?
-    if ((size_t)real_len + 1 <= len) return -1;
-
-    int z_len = real_len;
+    char real_buf[COMPLEX_FORMAT_BUF_LEN];
+    char imag_buf[COMPLEX_FORMAT_BUF_LEN];
     
-    int imag_idx = real_len;
+    int real_len = fix9_23_format(z.re, real_buf,COMPLEX_FORMAT_BUF_LEN);
+    int imag_len = fix9_23_format(z.im, imag_buf,COMPLEX_FORMAT_BUF_LEN);
 
-    assert(imag_idx >= 0);
+    assert(real_len >= 0);
+    assert(imag_len >= 0);
 
-    if (z.im.raw >= 0) {
-        buffer[real_len] = '+';
-        imag_idx = real_len+1;
-        z_len++;
+    if(z.im.raw >= 0) {
+        return snprintf(buffer, len, "%.*s+%.*sj", real_len, real_buf, imag_len, imag_buf);
+    } else { 
+        return snprintf(buffer, len, "%.*s%.*sj", real_len, real_buf, imag_len, imag_buf);
     }
-
-    assert((int32_t)len - imag_idx - 1 >= 0);
-
-    int imag_len = fix9_23_format(z.im, &buffer[imag_idx], len - imag_idx - 1);
-    if (imag_len <= 0) return -1;
-    z_len += imag_len;
-
-    return z_len;
-
 }
