638c074530a5b01a22a266a8d33d5f3e201f2ed9
LauKH312 <laurits.k.hillemann@gmail.com>
2026-06-15 00:01:48 +0200

    Fixpoint performance test and test of FFT plot

--- firmware/Project_Trace/Core/Inc/test_suite.h
+++ firmware/Project_Trace/Core/Inc/test_suite.h
@@ -13,5 +13,6 @@ void test_suite(void);
 void test_output_framebuffer(void);
 void test_output_fixpoint_table(void);
 void test_output_FFT(void);
+void test_fixpoint_performance(void);
 
 #endif /* INC_TEST_SUITE_H_ */
